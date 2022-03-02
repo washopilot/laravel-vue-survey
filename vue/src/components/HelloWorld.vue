a76687edb43c673da151f33553de36c61d0b38ca
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-01 23:13:22 -0500

    tailwind templates to routes

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,7 +1,4 @@
-<script setup></script>
-
-<template>
-  <!--
+<!--
   This example requires Tailwind CSS v2.0+ 
   
   This example requires some changes to your config:
@@ -17,14 +14,15 @@
   }
   ```
 -->
+<template>
   <!--
-  This example requires updating your template:
+    This example requires updating your template:
 
-  ```
-  <html class="h-full bg-gray-50">
-  <body class="h-full">
-  ```
--->
+    ```
+    <html class="h-full bg-gray-50">
+    <body class="h-full">
+    ```
+  -->
   <div
     class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
   >
@@ -40,6 +38,7 @@
         </h2>
         <p class="mt-2 text-center text-sm text-gray-600">
           Or
+          {{ ' ' }}
           <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
             start your 14-day free trial
           </a>
@@ -55,7 +54,7 @@
               name="email"
               type="email"
               autocomplete="email"
-              required
+              required=""
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Email address"
             />
@@ -67,7 +66,7 @@
               name="password"
               type="password"
               autocomplete="current-password"
-              required
+              required=""
               class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
               placeholder="Password"
             />
@@ -103,20 +102,10 @@
             class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-              <!-- Heroicon name: solid/lock-closed -->
-              <svg
+              <LockClosedIcon
                 class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
                 aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              />
             </span>
             Sign in
           </button>
@@ -126,4 +115,12 @@
   </div>
 </template>
 
-<style scoped></style>
+<script>
+import { LockClosedIcon } from '@heroicons/vue/solid';
+
+export default {
+  components: {
+    LockClosedIcon,
+  },
+};
+</script>
